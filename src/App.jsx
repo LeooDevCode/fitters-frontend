18018d11f975526d4c64d4e27f61ee77e8421246
Leonel <leone.25lira@gmail.com>
2026-09-16 10:26:29 -0600

    Fix: agregar ruta raiz y catch-all que redirigen a login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './paginas/Login'
 import Registro from './paginas/Registro'
 import Rutinas from './paginas/Rutinas'
@@ -11,12 +11,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/rutinas" element={<RutaProtegida><Rutinas /></RutaProtegida>} />
         <Route path="/ejercicios" element={<RutaProtegida><Ejercicios /></RutaProtegida>} />
         <Route path="/progreso" element={<RutaProtegida><Progreso /></RutaProtegida>} />
         <Route path="/inicio" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
